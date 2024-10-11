7a9435480f08f43f70e792caa10a811cb0d93862
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-10-11 10:42:26 +0700

    comment

---
+++ app/src/main/java/com/example/basickotlin/Colllections.kt
@@ -0,0 +1,8 @@
+package com.example.basickotlin
+
+// list adalah kumpulan data yang disusun secara berurutan seperto antrian
+// list bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// list bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// list Read-only menggunakan listOf
+// list Mutable menggunakan mutableListOf
+
