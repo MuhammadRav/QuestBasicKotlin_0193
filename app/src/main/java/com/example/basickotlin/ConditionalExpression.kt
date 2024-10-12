3e5f193b88acd347794e9310fa73116733b3b4a5
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-10-12 09:02:52 +0700

    salah file

--- app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
+++ app/src/main/java/com/example/basickotlin/ConditionalExpression.kt
@@ -1,2 +1,72 @@
 package com.example.basickotlin
 
+import java.util.concurrent.locks.AbstractQueuedSynchronizer.ConditionObject
+
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+
+    // if-else
+    val number = 10
+    if (number > 0){
+        println("Positif Number")
+    } else {
+        println("Negative Number")
+    }
+
+    // when
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+    // Try Catch
+    val value = "10"
+    try{
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+}
+
+fun Perulangan(){
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5){
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars){
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5){
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main(){
+    ConditionalStatement()
+    Perulangan()
+}
+
