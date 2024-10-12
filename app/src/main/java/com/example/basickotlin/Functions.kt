3543241a4d83a8f65127dc7fa181a491bac1d071
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-10-12 16:30:52 +0700

    parameter

--- app/src/main/java/com/example/basickotlin/Functions.kt
+++ app/src/main/java/com/example/basickotlin/Functions.kt
@@ -1,2 +1,13 @@
 package com.example.basickotlin
 
+fun withoutParameter(){
+    println("== withoutParameter ==")
+    println("Hello World!")
+}
+
+fun withParameter(name: String){
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
