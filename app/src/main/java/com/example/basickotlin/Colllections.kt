0fdd5c792200bc53a8564aec41b2b0c45aedfc9c
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-10-12 08:28:31 +0700

    read only n mutable map

--- app/src/main/java/com/example/basickotlin/Colllections.kt
+++ app/src/main/java/com/example/basickotlin/Colllections.kt
@@ -71,6 +71,17 @@ fun ContohSet(){
 // Map Mutable menggunakan mutableMapOf
 
 fun ContohMap(){
+    println()
+    println("=== Map ===")
+
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle"to 1, "Squar" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map mutable
+    val shape: MutableMap<String, Int> = mutableMapOf(("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
 
 }
 
